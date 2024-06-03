82326f0de355d8f17329e1c0918a32fe9cffcf02
yunchocopie <yunnn687@naver.com>
2024-06-03 17:37:20 +0900

    OAuth Kakao Credential Login 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:oauthapp/_core/move.dart';
 import 'ui/login_page/login_page.dart';
 
 GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
+    // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
+    WidgetsFlutterBinding.ensureInitialized();
+
+    // runApp() 호출 전 Flutter SDK 초기화
+    KakaoSdk.init(nativeAppKey: 'b545c60ee4c1c822251dc3b08c716f17',);
   runApp(ProviderScope(child: MyApp()));
 }
 
--- lib/ui/login_page/login_page.dart
+++ lib/ui/login_page/login_page.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:oauthapp/_core/http.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Login Page"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("카카오 로그인"),
+      ),
+      body: ElevatedButton(
+        child: Text("카카오로그인"),
+        onPressed: () async {
+          kakaoLogin();
+        },
+      ),
     );
+  }
+
+  void kakaoLogin() async {
+    try {
+      // 1. 크리덴셜 로그인 - 토큰 받기
+      OAuthToken token = await UserApi.instance.loginWithKakaoAccount();
+      // Bl_yCbOpzJe4vQGNCBX_cQI0VVBvdUm3AAAAAQo9dNsAAAGP3IxzfMYNwJ_muSR4
+      print('카카오계정으로 로그인 성공 ${token.accessToken}');
+
+      // 2. 토큰(카카오)을 스프링서버에 전달하기 (스프링 서버한테 나 인증했어!! 라고 알려주는것)
+      final response = await dio.get("/oauth/callback",
+          queryParameters: {"accessToken": token.accessToken});
+
+      // 3. 토큰(스프링서버) 응답받기
+      final blogAccessToken = response.headers["Authorization"]!.first;
+      print("blogAccessToken : ${blogAccessToken}");
+
+      // 4. 시큐어 스토리지에 저장
+      secureStorage.write(key: "blogAccessToken", value: blogAccessToken);
+
+      // 5. static, const 변수, riverpod 상태관리 (생략)
+    } catch (error) {
+      print('카카오계정으로 로그인 실패 $error');
+    }
   }
 }
